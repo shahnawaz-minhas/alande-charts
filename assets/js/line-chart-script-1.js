5ec9039a380ce94ec9849ef88fcc0079908caf30
Shah Nawaz <ch.shahnawaz1992@gmail.com>
2018-04-22 22:10:16 +0500

    changes in line chart to check it's responsiveness

--- assets/js/line-chart-script-1.js
+++ assets/js/line-chart-script-1.js
@@ -1,331 +1,124 @@
-
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-	width = 960 - margin.left - margin.right,
-	height = 500 - margin.top - margin.bottom;
-
-// parse the date / time
-var parseTime = d3.timeParse("%d-%b-%y");
-var formatTime = d3.timeFormat("%e %B");
-
-// set the ranges
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
-
-// define the line
-var valueline = d3.line()
-				  .x(function(d) { return x(d.entry_date); })
-				  .y(function(d) { return y(d.score); });
-
-var div = d3.select("body").append("div")
-			.attr("class", "tooltip")
-			.style("opacity", 0);
-
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-			.attr("width", width + margin.left + margin.right)
-			.attr("height", height + margin.top + margin.bottom)
-			.append("g")
-			.attr("transform",
-				"translate(" + margin.left + "," + margin.top + ")");
-
-// Get the data
-let data = [
-	{
-		"score"     : 125,
-		"entry_date": "2019-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2019-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2012-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2013-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2015-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2014-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2014-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2012-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2118-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2218-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2318-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2218-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2418-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2518-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2618-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2718-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2818-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2918-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2218-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2018-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2018-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2018-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2018-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2018-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2018-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2018-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2018-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2018-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2018-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2018-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2018-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2018-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2018-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2018-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2018-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2018-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2018-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2018-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2018-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2018-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2018-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2018-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2018-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2018-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2018-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2018-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2018-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2018-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2018-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2018-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2018-03-27"
-	}, {
-		"score"     : 125,
-		"entry_date": "2018-04-10"
-	},
-	{
-		"score"     : 20,
-		"entry_date": "2018-04-06"
-	},
-	{
-		"score"     : 115,
-		"entry_date": "2018-04-05"
-	},
-	{
-		"score"     : 130,
-		"entry_date": "2019-03-28"
-	},
-	{
-		"score"     : 160,
-		"entry_date": "2019-03-27"
-	}
+//////////////////////////////////////////////
+// Data //////////////////////////////////////
+//////////////////////////////////////////////
+// fake data
+var data = [
+	{year: 2000, population: 1192},
+	{year: 2001, population: 1234},
+	{year: 2002, population: 1463},
+	{year: 2003, population: 1537},
+	{year: 2004, population: 1334},
+	{year: 2005, population: 1134},
+	{year: 2006, population: 1234},
+	{year: 2007, population: 1484},
+	{year: 2008, population: 1562},
+	{year: 2009, population: 1427},
+	{year: 2010, population: 1325},
+	{year: 2011, population: 1484},
+	{year: 2012, population: 1661},
+	{year: 2013, population: 1537},
+	{year: 2014, population: 1334},
+	{year: 2015, population: 1134},
+	{year: 2016, population: 1427}
 ];
-	
-	// format the data
-	data.forEach(function(d) {
-		d.entry_date = parseTime(d.entry_date);
-		d.score = +d.score;
+
+// Parse the date / time
+var parseDate = d3.timeParse("%Y");
+
+// force types
+function type(dataArray) {
+	dataArray.forEach(function(d) {
+		d.year = parseDate(d.year);
+		d.retention = +d.population;
 	});
+	return dataArray;
+}
+data = type(data);
+
+//////////////////////////////////////////////
+// Chart Config /////////////////////////////
+//////////////////////////////////////////////
+
+// Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+	width, // width gets defined below
+	height = 250 - margin.top - margin.bottom;
+
+// Set the scales ranges
+var xScale = d3.scaleTime();
+var yScale = d3.scaleLinear().range([height, 0]);
+
+// Define the axes
+var xAxis = d3.axisBottom().scale(xScale);
+var yAxis = d3.axisLeft().scale(yScale);
+
+// create a line
+var line = d3.line();
+
+// Add the svg canvas
+var svg = d3.select("body")
+			.append("svg")
+			.attr("height", height + margin.top + margin.bottom);
+
+var artboard = svg.append("g")
+				  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// set the domain range from the data
+xScale.domain(d3.extent(data, function(d) { return d.year; }));
+yScale.domain([
+	d3.min(data, function(d) { return Math.floor(d.population - 200); }),
+	d3.max(data, function(d) { return Math.floor(d.population + 200); })
+]);
+
+// draw the line created above
+var path = artboard.append("path").data([data])
+				   .style('fill', 'none')
+				   .style('stroke', 'steelblue')
+				   .style('stroke-width', '2px');
+
+// Add the X Axis
+var xAxisEl = artboard.append("g")
+					  .attr("transform", "translate(0," + height + ")");
+
+// Add the Y Axis
+// we aren't resizing height in this demo so the yAxis stays static, we don't need to call this every resize
+var yAxisEl = artboard.append("g")
+					  .call(yAxis);
+
+//////////////////////////////////////////////
+// Drawing ///////////////////////////////////
+//////////////////////////////////////////////
+function drawChart() {
+	// reset the width
+	width = parseInt(d3.select('body').style('width'), 10) - margin.left - margin.right;
 	
-	// scale the range of the data
-	x.domain(d3.extent(data, function(d) { return d.entry_date; }));
-	y.domain([0, d3.max(data, function(d) { return d.score; })]);
+	// set the svg dimensions
+	svg.attr("width", width + margin.left + margin.right);
 	
-	// add the valueline path.
-	svg.append("path")
-	   .data([data])
-	   .attr("class", "line")
-	   .attr("d", valueline);
+	// Set new range for xScale
+	xScale.range([0, width]);
 	
-	// add the dots with tooltips
-	svg.selectAll("dot")
-	   .data(data)
-	   .enter().append("circle")
-	   .attr("r", 5)
-	   .attr("cx", function(d) { return x(d.entry_date); })
-	   .attr("cy", function(d) { return y(d.score); })
-	   .on("mouseover", function(d) {
-		   div.transition()
-			  .duration(200)
-			  .style("opacity", .9);
-		   div.html(formatTime(d.entry_date) + "<br/>" + d.score)
-			  .style("left", (d3.event.pageX) + "px")
-			  .style("top", (d3.event.pageY - 28) + "px");
-	   })
-	   .on("mouseout", function(d) {
-		   div.transition()
-			  .duration(500)
-			  .style("opacity  ", 0);
-	   });
+	// give the x axis the resized scale
+	xAxis.scale(xScale);
 	
-	// add the X Axis
-	svg.append("g")
-	   .attr("transform", "translate(0," + height + ")")
-	   .call(d3.axisBottom(x));
+	// draw the new xAxis
+	xAxisEl.call(xAxis);
 	
-	// add the Y Axis
-	svg.append("g")
-	   .call(d3.axisLeft(y));
+	// specify new properties for the line
+	line.x(function(d) { return xScale(d.year); })
+		.y(function(d) { return yScale(d.population); });
 	
+	// draw the path based on the line created above
+	path.attr('d', line);
+}
+
+// call this once to draw the chart initially
+drawChart();
+
+
+//////////////////////////////////////////////
+// Resizing //////////////////////////////////
+//////////////////////////////////////////////
+
+// redraw chart on resize
+window.addEventListener('resize', drawChart);
